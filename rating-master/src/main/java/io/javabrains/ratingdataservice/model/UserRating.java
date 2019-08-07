0f53cad170878dfdc6f97322a8b0d9aebc8662a7
kailashsahooc <44028236+kailashsahooc@users.noreply.github.com>
2019-08-07 07:55:26 +0530

    Add files via upload

---
+++ rating-master/src/main/java/io/javabrains/ratingdataservice/model/UserRating.java
@@ -0,0 +1,14 @@
+package io.javabrains.ratingdataservice.model;
+import java.util.*;
+
+public class UserRating {
+    private List<Rating> userRating;
+
+    public List<Rating> getUserRating() {
+        return userRating;
+    }
+
+    public void setUserRating(List<Rating> userRating) {
+        this.userRating = userRating;
+    }
+}
